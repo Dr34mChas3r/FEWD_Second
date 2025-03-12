cf600c8659dcff081985ac81101e86f9b3e80d36
Sergiy@sergi <serhii.zabachynskyi-ki241@nung.edu.ua>
2025-03-12 21:04:12 +0200

    Initial commit

---
+++ LW1/main.js
@@ -0,0 +1,16 @@
+
+console.log("~~~~Variant №6~~~~")
+// task 1.2 (6)
+console.log("Task 1.2")
+let x=-2;
+let y=5;
+let result=(x*y)<=0&&y>=4;
+console.log(result)
+// task 2.1 
+console.log("Task 2.1")
+let a=0.7;
+let b=-4;
+y=Math.log(Math.abs((Math.PI/a)-b))+Math.pow(Math.tan(Math.pow(b),3),2) 
+x=21.4*Math.pow((a-0.5),2)+Math.cos(Math.PI/b)
+epsylon=Math.pow(Math.exp,2)*Math.log2(Math.pow(x,4))-Math.sqrt(Math.abs(y+1));
+console.log(epsylon)
