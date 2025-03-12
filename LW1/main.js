c8bbee136f8f8d70249375ef7bb623cb7bfe4ebe
Dr34mChas3r <sergiyzabach512@gmail.com>
2025-03-12 22:01:11 +0200

    Done work
    Made 1-5 tasks, and seems it`s fine working

--- LW1/main.js
+++ LW1/main.js
@@ -10,11 +10,47 @@ console.log(result)
 console.log("Task 2.1")
 a=0.7;
 b=-4;
-y=Math.log(Math.abs((Math.PI/a)-b))+Math.pow(Math.tan(Math.pow(b),3),2)
-let debug_test=Math.pow(Math.tan(Math.pow(b)),3)
-console.log(") ="+debug_test)
-console.log("y="+y)
+y=Math.log(Math.abs((Math.PI/a)-b))+Math.pow(Math.tan(Math.pow(b,3)),2)
 x=21.4*Math.pow((a-0.5),2)+Math.cos(Math.PI/b)
-console.log("x="+x)
-epsylon=Math.pow(Math.exp,2)*Math.log2(Math.pow(x,4))-Math.sqrt(Math.abs(y+1));
+epsylon=Math.exp(2)*Math.log2(Math.pow(x,4))-Math.sqrt(Math.abs(y+1));
 console.log(epsylon)
+//Task 3
+console.log("task 3.1")
+x=1 // змінну задає користувач :) 
+if(x<=-1){
+    y=Math.atan(x)
+} else if (Math.abs(x)<1){
+    y=log10(Math.sqrt(1-Math.pow(x,2)))
+} else if (x>=1){
+    y=Math.exp(-Math.abs(x))
+}
+console.log(y)
+//Task 4.1
+console.log("Task 4.1.a")
+for(let x=0.1;x<=0.9;x+=0.1){
+    y=(Math.acos(x))/((2*x)+1)
+    console.log(y)
+}
+console.log("Task 4.1.b")
+x=0;
+for(let i=0;i<4;i++){
+    y=(Math.acos(x))/((2*x)+1)
+    x+=0.2
+    console.log(y)
+}
+// Task 5.1
+console.log("Task 5")
+console.log("Sum")
+let S=0;
+for(let k=6;k<=18;k++){
+    S+=(Math.pow(k,4)+2)/(2*Math.pow(k,2)-1)
+}
+console.log(S)
+console.log("Dobutok")
+let F=1;
+r=3
+m=9
+for(n=r;n<=m;n++){
+    F*=n/((n+2)*(n+5))
+}
+console.log(F)
